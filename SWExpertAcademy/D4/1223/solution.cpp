a4e4be9ebacec9f45772ec0641ce066b225a3a64
jeha0714 <whrydus0714@gmail.com>
2026-02-08 13:18:15 -0800

    FEAT: 20260208 SW Expert Academy Coding Problem

---
+++ SWExpertAcademy/D4/1223/solution.cpp
@@ -0,0 +1,140 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int get_operator_priority(char op) {
+    switch (op) {
+        case '+' :
+            return 1;
+        case '-' :
+            return 1;
+        case '*' :
+            return 2;
+        case '/' :
+            return 2;
+        default :
+            return 0;
+    }
+}
+
+int main(int argc, char** argv)
+{
+	int         test_case;
+    int         len;
+    string      infix;
+    string      postfix;
+    stack<int>  st;
+    int         tmp1, tmp2;
+    int         result;
+	/*
+	   아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
+	   앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+	   //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+	   freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+	   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
+	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
+	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
+	*/
+	//freopen("input.txt", "r", stdin);
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= 10; ++test_case)
+	{
+        // 0. Init info
+            // a. clear
+        infix.clear();
+        postfix.clear();
+        while (!st.empty())
+            st.pop();
+            // b. input
+        cin >> len >> infix;
+
+        // 1. Convert infix to postfix
+        for (int i = 0; i < len; i++) {
+            // if infix[i] is digit
+            if (isdigit(infix[i])) {
+                postfix.push_back(infix[i]);
+            }
+            // if infix[i] is operator
+            else {
+                if (st.empty()) {
+                    st.push(infix[i]);
+                }
+                else { // stack not empty
+                    int cur_priority = get_operator_priority(infix[i]);
+                    while (!st.empty() && get_operator_priority(st.top()) >= cur_priority) {
+                        postfix.push_back(st.top());
+                        st.pop();
+                    }
+                    st.push(infix[i]);
+                }
+            }
+        }
+        while (!st.empty()) {
+            postfix.push_back(st.top());
+            st.pop();
+        }
+
+        // 2. Calculate postfix
+        for (int i = 0; i < len; i++) {
+            // if postfix[i] is digit
+            if (isdigit(postfix[i])) {
+                st.push(postfix[i] - '0');
+            }
+            // if postfix[i] is operator
+            else {
+                tmp1 = st.top();
+                st.pop();
+                tmp2 = st.top();
+                st.pop();
+                if (postfix[i] == '+')
+                    st.push(tmp1 + tmp2);
+                else if (postfix[i] == '*')
+                    st.push(tmp1 * tmp2);
+            }
+        }
+        result = st.top();
+        st.pop();
+
+        // 3. Print Result
+        cout << "#" << test_case << " ";
+        cout << result;
+        cout << "\n";
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}

d74762f423fd37aa8c29a580f805389a889b309b
jeha0714 <whrydus0714@gmail.com>
2026-02-14 20:29:08 -0800

    FEAT: 20260214 SW Expert Academy Coding Problem

---
+++ SWExpertAcademy/D4/1238/solution.cpp
@@ -0,0 +1,105 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+#define NONVISITED 0
+
+void    bfs(int node, queue<int> & q, vector<int> & visited, vector<vector<int> > & route) {
+
+    for (int i = 0; i < route[node].size(); i++) {
+        if (visited[route[node][i]] == NONVISITED) {
+            q.push(route[node][i]);
+            visited[route[node][i]] = visited[node] + 1;
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
+	int                     test_case;
+    int                     n, start_node;
+    int                     s, e;
+    vector<int>             visited(101, NONVISITED);
+    vector<vector<int> >    route(101, vector<int>());
+    queue<int>              q;
+	/*
+	   아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
+	   앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+	   //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+	   freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+	   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
+	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
+	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
+	*/
+	//freopen("input.txt", "r", stdin);
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= 10; ++test_case)
+	{
+        // 0. Init
+        for (int i = 1; i <= 100; i++) {
+            visited[i] = NONVISITED;
+            route[i].clear();
+        }
+        cin >> n >> start_node;
+        for (int i = 0; i < (n >> 1); i++) {
+            cin >> s >> e;
+            route[s].push_back(e);
+        }
+        visited[start_node] = 1;
+        q.push(start_node);
+
+        // 1. Start calling
+        while (!q.empty()) {
+            s = q.front();
+            q.pop();
+            bfs(s, q, visited, route);
+        }
+        for (int i = 1; i <= 100; i++) {
+            if (visited[i] != NONVISITED && visited[i] >= visited[s]) {
+                s = i;
+            }
+        }
+
+        // 2. Print result
+        cout << "#" << test_case << " ";
+        cout << s;
+        cout << "\n";
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
